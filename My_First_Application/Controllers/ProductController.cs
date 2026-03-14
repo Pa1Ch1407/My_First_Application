94a74ada00788d584b7f86b51fec0635792c1fc5
Pa1Kasyap <pavankasyap91@gmail.com>
2026-03-14 07:35:21 +0530

    Route and model binding demo

--- My_First_Application/Controllers/ProductController.cs
+++ My_First_Application/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using My_First_Application.Iservices;
 using My_First_Application.Models.DTO;
 
@@ -14,18 +16,70 @@ namespace My_First_Application.Controllers
         {
             _productService = productService;
         }
-        [HttpGet]
+        [HttpGet("GetProductList")]
+        [HttpGet("ProductList")]
+        [HttpGet("Product")]
         public ActionResult<IEnumerable<ProductDTO>> GetAllproducts()
         {
             var result = _productService.GetAllProduct();
             return Ok(result);
         }
 
+        //[HttpPost("Createproduct")]
+        //[HttpPost("NewProduct")]
+        //[HttpPost("GenerateProduct")]
+        [Route("createProduct")]
         [HttpPost]
         public ActionResult<ProductDTO> AddProduct(ProductCreateDTO createDTO)
         {
             var result = _productService.CreateProduct(createDTO);
-            return Ok(result);
+            return Created();
         }
+        #region Modeb binding
+        //[Route("{id}")]
+        //[HttpGet]
+        //public string GetId(int id)
+        //{
+        //    return $"Result Id: {id}";
+        //}
+
+        //Model Binding From Query
+        //[HttpGet("users")]
+        //public IActionResult GetUsers([FromQuery] int id, [FromQuery] string name)
+        //{
+        //    return Ok($"id: {id}, Name: {name}");
+        //}
+
+        //Model Binding From Route
+        //[HttpGet("users/{id}/{name}")]
+        //public IActionResult getUserbyId([FromRoute] int id, [FromRoute] string name)
+        //{
+        //    return Ok($"Id is: {id}, Name: {name}");
+        //}
+
+        //Model Binding from Form
+        //public class UserForm
+        //{
+        //    public string Name { get; set; }
+        //    public IFormFile ProfileImage { get; set; }
+        //}
+
+        //[HttpPost("upload")]
+        //public IActionResult UploadUser([FromForm] UserForm user)
+        //{
+        //    return Ok(user.Name);
+        //}
+
+
+
+        //Model binding from Body
+        //[Route("createProduct")]
+        //[HttpPost]
+        //public ActionResult<ProductDTO> AddProduct(ProductCreateDTO createDTO)
+        //{
+        //    var result = _productService.CreateProduct(createDTO);
+        //    return Created();
+        //}
+        #endregion
     }
 }
